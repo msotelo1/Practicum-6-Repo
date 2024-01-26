e1d4490c490b5fbc7b7087cd5ad4387a4609979d
Chuck476 <cdierbach@towson.edu>
2024-01-26 17:38:45 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,32 @@
+// Main.java
+// Code for Typical Java Errors Practicum
 
+// ----------------------------------------------------------
+// This program will convert Celsius to Fahrenheit
+// ----------------------------------------------------------
+public class Main {
+  public static void main(String[] args) {
+    
+    // init
+    Scanner input = new Scanner(System.in);
+    
+    // program greeting
+    System.out.println(
+      "\nConverts temperatures in Celsius to Fahrenheit\n");
+
+    // prompt for input
+    System.out.println("Enter temperature (in Celsius): ");
+    cel_temp = input.next();
+
+    // display results
+    System.out.println(cel_temp + " Celsius = " + 
+                       convertToFahrenheit(cel_temp) +
+                       " degrees Fahrenheit");
+  
+  }
+
+  public static double convertToFahrenheit(int c) {
+    return (9/5 * c) + 32;
+  }
+  
+}
